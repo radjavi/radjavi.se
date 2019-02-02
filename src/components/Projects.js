4e12c9d89aa25fc849fea0215e77872a84c79542
Iman Radjavi <radjavi@hotmail.com>
2019-02-02 12:54:39 +0100

    projects grid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,23 @@ class Projects extends Component {
   render() {
     return (
       <div className="Projects">
-        
+        <div style={{width: "100%", position: "relative"}}>
+          <div className="smallBody">
+            <div className="projects-text">
+              <h2>Selected Projects</h2>
+              <p id="projects-desc">Some of my favourite projects from the past year.</p>
+            </div>
+          </div>
+
+          <span style={{height: "420px", display: "block"}}></span>
+
+          <div className="projects-grid">
+            <div className="project-div"></div>
+            <div className="project-div"></div>
+            <div className="project-div"></div>
+            <div className="project-div"></div>
+          </div>
+        </div>
       </div>
     );
   }
