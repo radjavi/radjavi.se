c756fccef74ceddbe6ff6fab54fdf87b5014448d
Iman Radjavi <radjavi@hotmail.com>
2019-02-10 14:55:44 +0100

    page component for modularity

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,8 +6,15 @@ import Particles from 'react-particles-js';
 import particles_eatabeat from '../assets/particles-eatabeat.json'
 import img_eatabeat from '../assets/eatabeat-logo.svg'
 
-class Home extends Component {
+class Page extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    var {title, subtitle, description, links, activeProject} = this.props;
+    if (!links) links = [];
+
     const PoseLogo = posed.div({
       hide: { opacity: 0, top: "10px" },
       show: {
@@ -35,20 +42,6 @@ class Home extends Component {
       props: { delay: 0 }
     });
 
-    const PoseLink = posed.a({
-      hide: { opacity: 0, top: "10px" },
-      show: {
-        opacity: 1,
-        top: "0px",
-        delay: ({i}) => (i * 100 + 600),
-        transition: {
-          opacity: {duration: 300, ease: 'linear'},
-          top: {duration: 500, ease: 'easeOut'}
-        }
-      },
-      props: { i: 0 }
-    });
-
     const PoseProject = posed.div({
       hide: { opacity: 0, top: "30px" },
       show: {
@@ -66,6 +59,20 @@ class Home extends Component {
       hover: {scale: 1.05, transition: {duration: 200, ease: 'easeOut'}}
     });
 
+    const PoseLink = posed.a({
+      hide: { opacity: 0, top: "10px" },
+      show: {
+        opacity: 1,
+        top: "0px",
+        delay: ({i}) => (i * 100 + 600),
+        transition: {
+          opacity: {duration: 300, ease: 'linear'},
+          top: {duration: 500, ease: 'easeOut'}
+        }
+      },
+      props: { i: 0 }
+    });
+
     return (
       <div className="Home">
         <div className="smallBody home-grid">
@@ -80,31 +87,35 @@ class Home extends Component {
 
             <div id="about-text-block">
               <FadeUpDiv initialPose="hide" pose="show" id="about-title-div">
-                <h3 id="about-title">Hey! 👋<br />I'm Iman Radjavi</h3>
+                <h3 id="about-title">
+                  {title}
+                </h3>
+              </FadeUpDiv>
+              <FadeUpDiv initialPose="hide" pose="show" delay="50" style={{position: "relative"}}>
+                <h4 id="about-subtitle">
+                  {subtitle}
+                </h4>
               </FadeUpDiv>
               <FadeUpDiv initialPose="hide" pose="show" delay="600" style={{position: "relative"}}>
                 <p id="about-desc">
-                  Currently studying <span style={{color: "#E16565"}}>Computer Science & Engineering</span> at Chalmers University of Technology in Gothenburg, Sweden.
-                  On my spare time I work with fun projects to learn new technologies.
+                  {description}
                 </p>
               </FadeUpDiv>
               <div id="links">
-                <PoseLink i="1" initialPose="hide" pose="show" href="mailto:radjavi@hotmail.com" className="link">Email</PoseLink>
-                <PoseLink i="2" initialPose="hide" pose="show" href="https://www.linkedin.com/in/iman-radjavi/" target="_blank" className="link">LinkedIn</PoseLink>
-                <PoseLink i="3" initialPose="hide" pose="show" href="https://github.com/radjavi" target="_blank" className="link">GitHub</PoseLink>
+                {links.map(link => <PoseLink i={link.nr} initialPose="hide" pose="show" href={link.href} target="_blank" className="link">{link.text}</PoseLink>)}
               </div>
               <span style={{display: "block", height: "200px"}}></span>
             </div>
           </div>
 
           <div className="projects-grid">
-            <PoseProject i="1" id="project1" initialPose="hide" pose="show" className="project-div" onClick={() => (<Redirect push to="/eatabeat" />)}>
+            <PoseProject i="1" active={activeProject == 1} id="project1" initialPose="hide" pose="show" className="project-div" onClick={() => (<Redirect push to="/eatabeat" />)}>
               <Particles params={particles_eatabeat} style={{height: "100%", width: "100%", position: "absolute", borderRadius: "10px"}} />
               <img src={img_eatabeat} style={{height: "30%", margin: "auto"}} />
             </PoseProject>
-            <PoseProject i="2" initialPose="hide" pose="show" className="project-div"></PoseProject>
-            <PoseProject i="3" initialPose="hide" pose="show" className="project-div"></PoseProject>
-            <PoseProject i="4" initialPose="hide" pose="show" className="project-div"></PoseProject>
+            <PoseProject i="2" active={activeProject == 2} initialPose="hide" pose="show" className="project-div"></PoseProject>
+            <PoseProject i="3" active={activeProject == 3} initialPose="hide" pose="show" className="project-div"></PoseProject>
+            <PoseProject i="4" active={activeProject == 4} initialPose="hide" pose="show" className="project-div"></PoseProject>
           </div>
         </div>
       </div>
@@ -112,4 +123,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default Page;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './components/Home';
+import Page from './components/Page';
 import * as serviceWorker from './serviceWorker';
 import { Route, Link, BrowserRouter } from 'react-router-dom'
 
@@ -10,11 +10,41 @@ const routing = (
     <Route
       render={ ({location}) => (
         <div>
-          <Route path="/" component={Home} />
+          <Route exact path="/" render={() => homePage} />
+          <Route exact path="/eatabeat" render={() => eatabeatPage} />
         </div>
       )}
     />
   </BrowserRouter>
+)
+
+const homePage = (
+  <Page
+    title={["Hey! 👋", <br />, "I'm Iman Radjavi"]}
+    description={[
+      "Currently studying ",
+      <span style={{color: "#E16565"}}>Computer Science & Engineering</span>,
+      " at Chalmers University of Technology in Gothenburg, Sweden. On my spare time I work with fun projects to learn new technologies."
+    ]}
+    links={[
+      {nr: 1, text: "Email", href: "mailto:radjavi@hotmail.com"},
+      {nr: 2, text: "LinkedIn", href: "https://www.linkedin.com/in/iman-radjavi/"},
+      {nr: 3, text: "GitHub", href: "https://github.com/radjavi"}
+    ]}
+  />
+)
+
+const eatabeatPage = (
+  <Page
+    title="Eat A Beat"
+    subtitle="Spotify devX 2018 - Hackathon, Gothenburg"
+    description="A 2D game generated from any track, powered by the Spotify API/SDK. This project won 1st place on Spotify DevX Hackathon 2018."
+    activeProject="1"
+    links={[
+      {nr: 1, text: "View", href: ""},
+      {nr: 2, text: "Source", href: ""},
+    ]}
+  />
 )
 
 ReactDOM.render(routing, document.getElementById('root'));
